080b8d98328074c4fb93e6048b1d4cf6885c0a1f
Aloneg <kacper141414@gmail.com>
2024-04-03 23:40:58 +0200

    refactored fked up sigwaits to sleep, added critical sections locks (sigmasks), added flag defines

--- src/daemon.h
+++ src/daemon.h
@@ -18,9 +18,17 @@
 
 #define __file_seeker_max_arg_len 127
 
+#define flag_sleep 0
+#define flag_start 1
+#define flag_stop 2
+#define flag_scan 3
+#define flag_termination 4
+
 int print_usage(FILE* stream, int exit_code);
 int overlord(int argc, char**argv);
 void options_handler(int argc, char** argv);
 int create_subdaemons(int argc);
+void critical_lock(int sig);
+void critical_unlock(int sig);
 
 #endif
